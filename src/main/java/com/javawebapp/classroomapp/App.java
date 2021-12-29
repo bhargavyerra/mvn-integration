131d7b2d80f14047911d3403b4a25dfb9b851ed1
bhargavyerra <96267424+bhargavyerra@users.noreply.github.com>
2021-12-28 23:47:59 -0500

    Add files via upload

---
+++ src/main/java/com/javawebapp/classroomapp/App.java
@@ -0,0 +1,13 @@
+package com.javawebapp.classroomapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
